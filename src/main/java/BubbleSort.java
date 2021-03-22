107e8984ce6781ebb83fd01f1d0a9acb93427813
Никита <grand.marchello@gmail.com>
2021-03-22 18:44:10 +0400

    Решение задания 3

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,4 +1,22 @@
+import java.util.Arrays;
 
 public class BubbleSort {
-
+static int[] bubbleSort(int[] arr, int length){
+    boolean check = true;
+    for (int i = 1; i < length;i++){
+        if (arr[i] < arr[i - 1]){
+            int temp = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1] = temp;
+            check = false;
+        }
+    }if (!check){bubbleSort(arr,length);}
+    return arr;
 }
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        arr = bubbleSort(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
